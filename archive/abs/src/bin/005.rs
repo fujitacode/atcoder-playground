8d168a24c1851e32e6b3ef8cc3617f66a1471d4c
fujitacode <riot.jack.bbb@gmail.com>
2026-03-01 23:03:59 +0900

    005~007まで解く

--- archive/abs/src/bin/005.rs
+++ archive/abs/src/bin/005.rs
@@ -2,10 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        a: i64,
+        b: i64,
+        c: i64,
+        x: i64
     }
 
-    let ans = n
+    let mut count = 0;
 
-    println!("{}", ans);
+    for i in 0..= a {
+        for j in 0..= b {
+            for k in 0..= c {
+                if 500 * i + 100 * j + 50 * k == x {
+                    count += 1;
+                } 
+            }
+        }
+    }
+
+    println!("{}", count);
 }
---
+++ archive/abs/src/bin/007.rs
@@ -0,0 +1,27 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: i64,
+        b: i64
+    }
+
+    let mut total = 0;
+
+    for i in 1..=n {
+        let mut x = i;
+        let mut sum = 0;
+
+        while x > 0 {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        if a <= sum && sum <= b {
+            total += i;
+        }
+    }
+
+    println!("{}", total);
+}
