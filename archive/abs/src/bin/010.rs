440dbbbfa8b4536b159c2f68e1c787423724c81a
fujitacode <riot.jack.bbb@gmail.com>
2026-03-04 00:22:55 +0900

    10 11 AC

--- archive/abs/src/bin/010.rs
+++ archive/abs/src/bin/010.rs
@@ -2,10 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        mut s: String,
     }
 
-    let ans = n
+    while !s.is_empty() {
+        if s.ends_with("dreamer") {
+            s.truncate(s.len() - 7);
+        } else if s.ends_with("dream") {
+            s.truncate(s.len() - 5);
+        } else if s.ends_with("eraser") {
+            s.truncate(s.len() - 6);
+        } else if s.ends_with("erase") {
+            s.truncate(s.len() - 5);
+        } else {
+            println!("NO");
+            return
+        }
+    }
 
-    println!("{}", ans);
+    println!("YES");
 }
--- archive/abs/src/bin/011.rs
+++ archive/abs/src/bin/011.rs
@@ -5,7 +5,29 @@ fn main() {
         n: usize,
     }
 
-    let ans = n
+    let mut prev_t: i64 = 0;
+    let mut prev_x: i64 = 0;
+    let mut prev_y: i64 = 0;
 
-    println!("{}", ans);
+    for _ in 0..n {
+        input! {
+            t: i64,
+            x: i64,
+            y: i64,
+        }
+
+        let dt = t - prev_t;
+        let dist = (x - prev_x).abs() + (y - prev_y).abs();
+
+        if dt < dist || (dt - dist) % 2 != 0 {
+            println!("No");
+            return;
+        }
+
+        prev_t = t;
+        prev_x = x;
+        prev_y = y;
+    }
+
+    println!("Yes");
 }
