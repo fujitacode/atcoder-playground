cac0d1cefe89c1e7ea7b2114c357d1183d076ccc
fujitacode <riot.jack.bbb@gmail.com>
2026-06-22 23:22:39 +0900

    #4 開発環境を整備

--- ts/abs/001.ts
+++ ts/abs/001.ts
@@ -1,7 +1,8 @@
 import * as fs from 'fs';
 
-const input = require('fs').readFileSync(0, 'utf8').split(' ');
+const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/);
+
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
 
-console.log((a * b) % 2 == 0 ? 'Even' : 'Odd');
+console.log((a * b) % 2 === 0 ? "Even" : "Odd");
