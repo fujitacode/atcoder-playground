1265ef3f68a0dc40db54d03e87801372e5113c41
fujitacode <riot.jack.bbb@gmail.com>
2026-06-22 22:28:00 +0900

    #4 開発環境を更新

--- rs/abs/001.rs
+++ rs/abs/001.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let v: Vec<i32> = input.split_whitespace()
+                           .map(|s| s.parse().unwrap())
+                           .collect();
+    let (a, b) = (v[0], v[1]);
+
+    if (a * b) % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
 }
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,1 +1,16 @@
-// ここが実行ファイルなので消さないでください。
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let v: Vec<i32> = input.split_whitespace()
+                           .map(|s| s.parse().unwrap())
+                           .collect();
+    let (a, b) = (v[0], v[1]);
+
+    if (a * b) % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
+}
